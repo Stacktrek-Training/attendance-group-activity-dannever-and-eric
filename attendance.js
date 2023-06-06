b99af8e60136f87178c55fc9d43d77e1bd8c37a5
esvillamor <70527961+esvillamor@users.noreply.github.com>
2023-06-06 17:53:39 +0800

    Upload attendance.js

---
+++ attendance.js
@@ -0,0 +1,25 @@
+function attendance(word) {
+  const trimmed = word.trim(); 
+  const toLower = trimmed.toLowerCase();
+  const presentWords = ['minahal', 'nasaktan', 'nagmahal'];
+  
+  return presentWords.includes(toLower) ? 'Present' : 'Absent';
+
+  /*
+    if (words.includes(word)) {
+      return 'Present';
+    } else {
+      return 'Absent';
+    }
+  */
+  }
+  
+  // Example usage:
+  console.log(attendance('Minahal'));     // Output: Present
+  console.log(attendance('Nasaktan'));    // Output: Present
+  console.log(attendance('Nagmahal'));    // Output: Present
+  console.log(attendance('NotPresent'));  // Output: Absent
+  console.log(attendance(' '));     // Output: Absent
+  console.log(attendance(' nasaktan '));    // Output: 
+  console.log(attendance('nagmahal'));    // Output: 
+  console.log(attendance('NotPresent'));  // Output:   
